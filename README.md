514696ead99a4b51aaafc0a634dba9ed5114007c
Johnny <johny60207@gmail.com>
2015-05-31 20:27:39 +0800

    Readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # spark_train
+
+1 因為找不太到spark提供的multiclass SVM，所以自己做簡單的版本，training時主要就是對每個class產生一個分類器，
+  test時，就把資料丟給每一個分類器，若label是1則預測的結果為該分類器所隸屬的label．若有多個分類器結果都是1，
+  就採用training data label的分佈數量最多的來決定這個不確定的label，若都沒有分類器的結果是1，則將此label設為
+  training data label的分佈數量最少的label．
+
+2 此題想到的方法是用先用PCA，對資料做降維，之後再用SVM對這個被降為的資料做training & test
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from pyspark.mllib.classification import SVMModel
-from pyspark.mllib.classification import SVMWithSGD
+#from pyspark.mllib.classification import SVMModel
 #from pyspark.mllib.classification import LogisticRegressionWithSGD
-from pyspark.mllib.classification import LogisticRegressionWithLBFGS
+#from pyspark.mllib.classification import LogisticRegressionWithLBFGS
+import numpy as np
+from sklearn.decomposition import PCA
+
+from pyspark.mllib.classification import SVMWithSGD
 from pyspark.mllib.regression import LabeledPoint
 from pyspark import SparkContext
 from pyspark.mllib.util import MLUtils
+import copy
 
 
 app_name = "WordCount"
@@ -47,7 +51,8 @@ def transform_to_data(rows):
         lp = LabeledPoint(*convert_features(row))
         data.append(lp)
         test.write("{0} {1}\n".format(lp.label, 
-                                      ' '.join(["{0}:{1}".format(i, lp.features[i]) for i in range(len(lp.features)) if lp.features[i] != 0])))
+                                      ' '.join([str(f) for f in lp.features])))
+                                      #' '.join(["{0}:{1}".format(i, lp.features[i]) for i in range(len(lp.features)) if lp.features[i] != 0])))
 
     test.close()
     return data
@@ -76,66 +81,101 @@ def read_file(file_name):
     fp.close()
     return rows
 
+def get_principal_indexes(data, required):
+    pca = PCA(n_components=len(data[0].features))
+    X = np.array([row.features for row in data])
+    pca.fit(X)
+    print pca.explained_variance_ratio_
+
+# Gen multiple classfiers
+def gen_predictors(training_data):
+    classifiers = dict()
+    for label in label_map.values():
+        training_data = copy.deepcopy(training_data)
+        for row in training_data:
+            if row.label == label: row.label = 1
+            else: row.label = 0
+
+        svm = SVMWithSGD.train(sc.parallelize(training_data))
+        classifiers[label] = svm
+
+    return classifiers
+
+def predict(test_data, classifiers, hist):
+    # Min count label(in training data)
+    min_label = hist.index(min(hist))
+    predicts = list()
+    for row in test_data:
+        belong = None
+        for label in label_map.values():
+            if classifiers[label].predict(row.features) == 1:
+                if belong is None: belong = label
+                elif hist[label] > hist[belong]: belong = label
+            
+        if belong is None: belong = min_label
+        predicts.append(belong)
+
+    return predicts
+
 def report(actuals, predicts):
     tp_count = dict()
     actual_count = dict()
     predict_count = dict()
 
     # Initialize the count
-    for label in labels:
+    for label in label_map.values():
         tp_count[label] = 0
         actual_count[label] = 0
         predict_count[label] = 0
 
     for i in range(len(actuals)):
-        label = labels[i]
+        actual = actuals[i]
         predict = predicts[i]
 
-        actual_count[label] += 1
+        actual_count[actual] += 1
         predict_count[predict] += 1
-        if predict == label:
-            tp_count[label] += 1
+        if predict == actual:
+            tp_count[actual] += 1
     
+    p_amount = 0
     precision = 0.0
+    r_amount = 0
     recall = 0.0
-    for label in labels:
-        precision += tp_count[label] / float(actual_count[label])
-        recall += tp_count[label] / float(predict_count[label])
+    for label in label_map.values():
+        if actual_count[label] != 0: 
+            precision += tp_count[label] / float(actual_count[label])
+            p_amount += 1
 
-    precision /= len(labels)
-    recall /= len(labels)
+        if predict_count[label] != 0: 
+            recall += tp_count[label] / float(predict_count[label])
+
+    print p_amount, r_amount
+    precision /= p_amount
+    recall /= r_amount
         
     print "Precision: {0}".format(precision)
     print "Recall: {0}".format(recall)
 
 
 if __name__ == "__main__":
-    #rows = read_file("10_percent.txt")
-    #data = transform_to_data(rows)
-    data = MLUtils.loadLibSVMFile(sc, "Con.txt").collect()
+    rows = read_file("10_percent.txt")
+    data = transform_to_data(rows)
+    #data = MLUtils.loadLibSVMFile(sc, "Con.txt").collect()
     #data = MLUtils.loadLibSVMFile(sc, "sample_libsvm_data.txt").collect()
 
-    test_data = data[100000:]
+    training_data = data[:10000]
+    test_data = data[:10000]
+    #test_data = data[10000:]
 
-    # Gen multiple classfiers
-    classifiers = dict()
-    for label in label_map.values():
-        train_data = copy.deepcopy(data[:100000])
-        for row in train_data
-            if row.label == label: row.label = 1
-            else: row.label = 0
-
-        svm = SVMWithSGD.train(sc.parallelize(train_data))
-        classifiers[label] = svm
-    
-    actuals = list()
-    predicts = list()
-    for row in test_data:
-        actuals.append(row.label)
+    hist = [0 for label in range(len(label_map))]
+    for label in sorted(label_map.values()):
+        hist[label] += 1
         
-        for label in label_map.values():
-            if classifiers[label].predict(row.features) == 1:
-                predicts.append(label)
-                break
-            
-    report(labels, predicts)
+    classifiers = gen_predictors(training_data)
+
+    #get_principal_indexes(training_data, required=30)
+
+    actuals = [row.label for row in test_data]
+    predicts = predict(test_data, classifiers, hist)
+
+    report(actuals, predicts)
